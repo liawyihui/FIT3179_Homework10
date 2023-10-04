ad67e9225770a12f4a343be1e1746259baf3c2c9
liawyihui <ylia0072@student.monash.edu>
2023-10-04 10:45:28 +0800

    added bubble

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -2,3 +2,8 @@ var world_map = "js/property_distribution.vg.json";
 vegaEmbed('#world_map', world_map, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var bubble_chart = "js/bubble_plot_area_rooms_price.vg.json";
+vegaEmbed('#bubble_chart', bubble_chart, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
